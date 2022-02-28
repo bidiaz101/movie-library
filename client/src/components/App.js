a70ed2febf48d1af967f5d39b10534259d6ee982
Brian Diaz <bidiaz101@gmail.com>
2022-02-28 15:05:02 -0600

    separated Header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import { Switch, Route, Link, useLocation, useHistory } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Browse from './Browse'
 import Navbar from './Navbar'
 import Signup from './Signup';
 import { useSelector, useDispatch } from 'react-redux';
-import { changeDarkMode, fetchUser } from '../features/user/userSlice';
-import { logout, login } from '../features/user/userSlice'
+import { fetchUser } from '../features/user/userSlice';
 import Login from './Login'
 import UserMovies from './UserMovies';
 import Search from './Search'
 import MoviePage from './MoviePage';
+import Header from './Header'
 import Home from './Home';
 
 function App() {
-  const { username, darkMode } = useSelector(state => state.user)
-  const userId = useSelector(state => state.user.id)
+  const { darkMode } = useSelector(state => state.user)
 
   const dispatch = useDispatch()
 
@@ -30,87 +29,11 @@ function App() {
     }
   }, [darkMode])
 
-  let signInBtns
-
-  switch(useLocation().pathname){
-    case '/login': 
-      signInBtns = <Link to='/signup'><button>Sign Up</button></Link>
-      break
-    case '/signup':
-      signInBtns = <Link to='/login'><button>Log In</button></Link>
-      break
-    default: 
-      signInBtns = (
-        <>
-          <Link to="/login">
-            <button>Login</button>
-          </Link>
-          <Link to="/signup">
-            <button>Sign Up</button>
-          </Link>
-        </>
-      )
-  }
-
-  const history = useHistory()
-
-  function handleLogout() {
-    fetch('/logout', { method: 'DELETE' })
-    .then(() => {
-      history.push('/')
-      dispatch(logout())
-    })
-  }
-
-  function handleGuest() {
-    fetch('/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        username: 'guest',
-        password: 'guest123'
-      })
-    })
-    .then(resp => resp.json())
-    .then(guestData => {
-      history.push('/')
-      dispatch(login(guestData))
-    })
-  }
-
-  function handleChange(){
-    fetch(`/users/${userId}`, {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        dark_mode: !darkMode
-      })
-    })
-    .then(dispatch(changeDarkMode()))
-  }
-
   const [moviePageId, setMoviePageId] = useState(0)
 
   return (
     <div>
-      <Link to='/'><h1 ><img id={darkMode ? 'logo-dark' : null} className='logo' src={require ('../logo.png')} alt='logo' />Super Movie Library</h1></Link>
-      <div className='button-container'>
-        {username ? (
-        <>
-          <p>{`Hey there, ${username}!`}</p>
-          <div>
-            <label htmlFor='darkMode' className='dark-mode-label'>{darkMode? 'Dark Mode:' : 'Light Mode'}</label>
-            <input type='checkbox' name='darkMode' checked={darkMode} onChange={handleChange} /> 
-          </div>
-          <button className={darkMode ? 'button-dark': null} onClick={handleLogout}>Logout</button>
-        </> 
-        ) : (
-        <>
-          {signInBtns}
-          <button className={darkMode ? 'button-dark': null} onClick={handleGuest}>Continue as Guest</button>
-        </>
-        )}
-      </div>
+      <Header />
       <Navbar />
       <Switch>
         <Route exact path="/">
---
+++ client/src/components/Header.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link, useHistory, useLocation } from 'react-router-dom'
+import { logout, login } from '../features/user/userSlice'
+import { changeDarkMode } from '../features/user/userSlice';
+
+
+function Header (){
+    const { username, darkMode } = useSelector(state => state.user)
+    const userId = useSelector(state => state.user.id)
+
+    const dispatch = useDispatch()
+    const history = useHistory()
+
+    function handleLogout() {
+        fetch('/logout', { method: 'DELETE' })
+        .then(() => {
+            history.push('/')
+            dispatch(logout())
+        })
+    }
+
+    function handleGuest() {
+        fetch('/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+            username: 'guest',
+            password: 'guest123'
+            })
+        })
+        .then(resp => resp.json())
+        .then(guestData => {
+            history.push('/')
+            dispatch(login(guestData))
+        })
+    }
+
+    function handleChange(){
+        fetch(`/users/${userId}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+            dark_mode: !darkMode
+            })
+        })
+        .then(dispatch(changeDarkMode()))
+    }
+
+    let signInBtns
+
+    switch(useLocation().pathname){
+        case '/login': 
+            signInBtns = <Link to='/signup'><button>Sign Up</button></Link>
+            break
+        case '/signup':
+            signInBtns = <Link to='/login'><button>Log In</button></Link>
+            break
+        default: 
+            signInBtns = (
+                <>
+                <Link to="/login">
+                    <button>Login</button>
+                </Link>
+                <Link to="/signup">
+                    <button>Sign Up</button>
+                </Link>
+                </>
+            )
+    }
+
+    return (
+        <div>
+            <Link to='/'><h1 ><img id={darkMode ? 'logo-dark' : null} className='logo' src={require ('../logo.png')} alt='logo' />Super Movie Library</h1></Link>
+            <div className='button-container'>
+                {username ? (
+                <>
+                <p>{`Hey there, ${username}!`}</p>
+                <div>
+                    <label htmlFor='darkMode' className='dark-mode-label'>{darkMode? 'Dark Mode:' : 'Light Mode'}</label>
+                    <input type='checkbox' name='darkMode' checked={darkMode} onChange={handleChange} /> 
+                </div>
+                <button className={darkMode ? 'button-dark': null} onClick={handleLogout}>Logout</button>
+                </> 
+                ) : (
+                <>
+                {signInBtns}
+                <button className={darkMode ? 'button-dark': null} onClick={handleGuest}>Continue as Guest</button>
+                </>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Header
