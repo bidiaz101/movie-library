05c8e887f4301eea9c5b2fb6dd16c863ea50edfe
Brian Diaz <bidiaz101@gmail.com>
2022-02-28 17:56:33 -0600

    moved dimensions of video to stylesheet

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,7 @@ function Home(){
         <div className='home'>
             <h2>What is Super Movie Library? (SML)</h2>
             <p>SML is an app designed to help users keep track of movies they own. It uses <a href='https://www.themoviedb.org/' target='_blank' rel="noreferrer">TMDB</a> API for all the data. See the video walkthrough below to learn more!</p>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/4Qoii_8cGOs" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe className='home-video' src="https://www.youtube.com/embed/4Qoii_8cGOs" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             <p>My name is Brian and you can find the code used to build this site <a href="https://github.com/bidiaz101/movie-library" target='_blank' rel="noreferrer">here!</a></p>
         </div>
     )
