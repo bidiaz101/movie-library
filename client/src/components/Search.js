e7979b6d565375425b349bc2f6ac91f3b43b17c8
Brian Diaz <bidiaz101@gmail.com>
2022-02-28 17:31:14 -0600

    added error message in case of no results

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -38,14 +38,14 @@ function Search(){
             <input id={darkMode ? 'option-dark' : null} type='text' onChange={handleChange} name='search' />
             <input type='submit' id={darkMode ? 'button-dark' : null} value='Go!' />
         </form>
-        {displayError ? <div className='app-wrap'><p>Please enter a search term</p></div> : null}
+        {displayError ? <div className='app-wrap'><h1>Please enter a search term</h1></div> : null}
         {results.length ? (
             <div className='grid-container'>
                 <div className='collection-grid'>
                     {moviesToDisplay}
                 </div>
             </div>
-         ) : null}
+         ) : <div className='app-wrap'><h1>No movies found</h1></div>}
         </>
 
     )
