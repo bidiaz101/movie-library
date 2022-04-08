4b2fe43de5c0eeba73cd13997c09a9f8a8caecc6
Brian Diaz <bidiaz101@gmail.com>
2022-04-08 01:29:01 -0500

    added styled pfp to reviews

--- client/src/components/ReviewCard.js
+++ client/src/components/ReviewCard.js
@@ -9,12 +9,17 @@ function ReviewCard({ review, handleDelete }){
     const darkMode = useSelector(state => state.user.darkMode)
 
     const buttonclassName = darkMode ? 'button-dark-review': 'button-review'
+
+    console.log(review)
     
     return (
          <div>
             <div>
                 <p>{stars}</p>
-                <h3>"{review.content}" - {review.user.username}</h3>
+                <div className='review-content'>
+                    <img src={review.user.profile_picture_thumbnail_url} alt='user profile picture' className='pfp-thumbnail' />
+                    <h3>"{review.content}" - {review.user.username}</h3>
+                </div>
                 {userId === review.user.id ? <button className={buttonclassName} onClick={() => handleDelete(review.id)} >Delete Review</button> : null}
             </div>
             <hr />
